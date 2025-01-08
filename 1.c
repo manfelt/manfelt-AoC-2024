66d5cdf2cc53fffc638eed89f0fdca2f80cd0ed7
Magnus Manfelt <35919896+manfelt@users.noreply.github.com>
2025-01-08 16:13:55 +0100

    it's working

--- 1.c
+++ 1.c
@@ -3,11 +3,12 @@
 #include <string.h>
 
 int main(void) {
+	int max = 99999;
 	FILE *file;
 	char line[1000];
 	int sum = 0;
 	int i = 1;
-	int distance;
+	int distance, lowa, lowb, lowest, j, indexa, indexb;
 
 	struct ab{
 		int a;
@@ -20,26 +21,49 @@ int main(void) {
 	file = fopen("1_input.txt", "r");
 
 	if (file == NULL) {
-		printf("Error opening file %s \n", file);
+		printf("Error opening file %p \n", file);
 		return 1;
 	}
 
 	while (fgets(line, sizeof(line), file) 	!= NULL) {
 		list[i].a = atoi(strtok(line, " ")); 
 		list[i].b = atoi(strtok(NULL, "\n"));
-		distance = abs(list[i].a - list[i].b);
-		sum += distance;
-		printf("# %d, a %d b %d, distance %d, sum  %d \n",i,list[i].a,list[i].b,distance, sum);
 		i++;
 		/* printf("%d \n",i); */
 
-	}	
-	
+	}		
 	fclose(file);
+	
+	lowa = lowb = max;
+	printf("lowa:%d \n",lowa);
+
+	for (i = 1; i <= sizeof(line); i++) {
+		for (j = 1; j <= sizeof(line); j++) {
+			if ( list[j].a <= lowa ) {
+				lowa = list[j].a;
+				indexa = j;	
+			
+			}
+		}
+		for (j = 1; j <= sizeof(line); j++) {
+			if ( list[j].b <= lowb ) {
+				lowb = list[j].b;
+				indexb = j;
+
+			}
+		}	
+		list[indexa].a = max;
+		list[indexb].b = max;
+
+		printf("%d %d\n",lowa, lowb);
+
+		distance = abs(lowa - lowb);
+		lowb = lowa = max;
+		sum += distance;
+	/*	printf("# %d, a %d b %d, distance %d, sum  %d \n",i,list[i].a,list[i].b,distance, sum); */
+	}
+
 
 	printf("\n ANSWER: %d \n", sum);
 
-
-
 	return 0;
-}
