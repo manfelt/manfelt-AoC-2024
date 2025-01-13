b843513a6fb1558a0598dd95b46641b1e720a89e
Magnus Manfelt <35919896+manfelt@users.noreply.github.com>
2025-01-13 15:06:11 +0100

    updated dynamic array functions

--- 2.c
+++ 2.c
@@ -32,7 +32,17 @@ int main(void) {
 	}
 
 	void insertArray(Array *a, int element) {
-		
+		if (a->used == a->size) {
+			a->size *= 2;
+			a->array = realloc(a->array, a->size * sizeof(int));
+		}
+		a->array[a->used++] = element;
+	}
+
+	void freeArray(Array *a) {
+		free(a->array);
+		a->array = NULL;
+		a->used = a->size = 0;
 	}
 
 	f = fopen("2_input.txt", "r");
