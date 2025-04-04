684053468cbe2e6dd7a64d6babce3b4bcb210bd1
Magnus Manfelt <35919896+manfelt@users.noreply.github.com>
2025-04-04 23:02:28 +0200

    Update 3.c

--- 3.c
+++ 3.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 1000
+
 char *string = "from()]mul(317,745)-+?;what()&{mul(67,323)select()~(+/}what()<mul(304,399)";
 char delim[] = ",";
 char *c, *p;
-int cur, num_a, num_b;
+int cur, rowlen, *pcur;
 // TODO char bol, row;
 
 struct Lex {
 	int cur;
 	int row;
-} Lexer;
+} Lexer = {0,0};
 
 struct operands {
 	int a;
@@ -37,28 +39,60 @@ char get_ch(int i, char *s) {
 	return result;
 }
 
-int expect_int(char i[]) {
-	printf("\n what %s",i);
-	// TODO strcmp() 
-	if (i=="1"||i=="2"||i=="3"||i=="4"||i=="5"||i=="6"||i=="7"||i=="8"||i=="9"||i=="0") {
+int expect_int(char i[]) { 
+	if (strcmp(i,"1")==0||strcmp(i,"2")==0||strcmp(i,"3")==0||strcmp(i,"4")==0||strcmp(i,"5")==0||strcmp(i,"6")==0||strcmp(i,"7")==0||strcmp(i,"8")==0||strcmp(i,"9")==0||strcmp(i,"0")==0) {
 		return 1;
 	} else {
 		return 0;
 	}
 }
 
+// unødvendig?
 int is_it_mul(int cur) {
 	if (1) {
-	return 1; 
+		return 1; 
 	}
 }
 
 int main() {
+	struct operands op[SIZE];
+	rowlen = strlen(string);
 	//loop_str(string);
 	cur = get_index(string,'m');
 	printf("index : %d \n",cur);
-	c = get_ch(cur,string);
-	printf("%c\n",c);
+	//c = get_ch(cur,string);
+	c = string[4];
+	printf("\n %c", c);
+
+
+	while (cur<rowlen) {
+		if (c==0) {
+			printf("\n m"); 
+			continue;
+		} else if (c=="u") {
+			printf("\n u");
+			continue;
+		} else if (c=="l") {
+			printf("\n l");
+			continue;
+		} else if (c=="(") {
+			printf ("\n (");
+			continue; 
+		}
+		cur++;
+	} 
+	/*
+	if (strcmp(c,"m")==0) {
+		if (strcmp(c,"u")==0) {
+			if (strcmp(c,"l")==0) 
+				if (strcmp(c,"(")==0) {
+				
+				} else { break; } 
+			} else { break; }
+		} else { break; }
+	} else { break; }
+	*/	
+
 	char a[] = "2";
 	if (strcmp("a","ab")) {printf("asd");}
 	if (expect_int(a)) {
