17e73020be0f55f8f1d27f4d0b0d41cd68a8200d
Magnus Manfelt <35919896+manfelt@users.noreply.github.com>
2025-05-27 22:31:53 +0200

    FINALLY
    Added memory allocation and file string input. Solved the puzzle.

--- 3.c
+++ 3.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 1000
+#define SIZE 100000
+#define DEBUG 0
 
-char *string = "from()]mul(317,745)-+?;what()&{mul(69,42,0)select()~(+/}what()<mul(304,399)";
-int delim_flag = 0;
+int delim_flag, res = 0;
 char *c, *p;
+FILE *f;
+char line[SIZE];
 int cur, rowlen, *pcur, mul_flag;
 // TODO char bol, row;
 
@@ -45,7 +47,7 @@ char get_ch(int i, char *s) {
 
 // TODO Gjør om til å motta character istedetfor string.
 int expect_int(char i[]) { 
-	printf("/n running expect_int with %s",i);
+	printf("\n running expect_int with %s",i);
 	if (strcmp(i,"1")==0||strcmp(i,"2")==0||strcmp(i,"3")==0||strcmp(i,"4")==0||strcmp(i,"5")==0||strcmp(i,"6")==0||strcmp(i,"7")==0||strcmp(i,"8")==0||strcmp(i,"9")==0||strcmp(i,"0")==0) {
 		return 1;
 	} else {
@@ -62,82 +64,139 @@ int is_it_mul(int cur) {
 
 char* concat(const char *s1, const char *s2)
 {
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
+	char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for null-terminator
+														// malloc errors check?
+	strcpy(result, s1);
+	strcat(result, s2);
+	return result;
+}
+
+char* memory_alloc(char *c) {
+	c = malloc(1 * sizeof(char));	
+	return c;
 }
 
 int main() {
 	struct operands op[SIZE];
-	rowlen = strlen(string);
-	//loop_str(string);
-	cur = get_index(string,'m');
-	printf("index : %d \n",cur);
+
+	//printf("index : %d \n",cur);
 	//*c = get_ch(cur,string);
 
 
-        // m 109
-        // u 117 
-        // l 108
-        // ( 40 
+	// m 109
+	// u 117 
+	// l 108
+	// ( 40 
 	// , 44
 	// ) 41
 
+	f = fopen("3_input.txt", "r");
+
+	if (f == NULL) {
+		printf("Error opening file %p \n", f);
+		return 1;
+	}	
+
+	int line_count = 1;
 	mul_flag = 0;
 	int inc = 0;
-	char *a = malloc(1 * sizeof(char));	
+	char *a = memory_alloc(a);
+	char *b = memory_alloc(b);	
 
+	if (DEBUG) {
+		printf("\n debug mode on");
+	} else {
+		printf("\n debug mode off");
+	}
 
-	while (cur<rowlen) {
-		cur++;
-		if (string[cur]==40) {
-			if (cur >= 3 && string[cur-1]==108 && string[cur-2]==117 && string[cur-3]==109) {
-				mul_flag = 1;
-				//struct str_val a = {.c = "0"};
-				//struct str_val b = {.c = "0"};
-								printf("\n mul( found at cur %d", cur);
-			};
-			continue; 
-		}
-		if (mul_flag) {
-			
-			printf("\n mul flag set, look for int or ','.");
-			char tmp = string[cur];
-			//b = concat(&tmp, a);
-			if (expect_int(&tmp)) {
-				printf("\n %c <- this is an integer.",string[cur]);
-				if(!delim_flag) {
-					inc++;
-					strcat(a,&tmp);
-					printf("\n value of 'a': %s",a);
-				} else {
-					//strcat(b,string[cur]);
-					//printf("\n value of 'b': %s",b);
-				}		}
-			 else if (string[cur]==44) {
-				printf("\n %c  delimeter. Expecting next argument",string[cur]);
-				if (!delim_flag) {
-					for (int i = 0; i > inc; i++) {
-						printf("\n USDHJFKLJS %c", string[cur-i]);
-					}
-					inc = 0;
-					delim_flag=1;
-				} else {
-					printf("\n unexpected token ',' at index %i. Break", cur);
-					mul_flag=0;
-					delim_flag=0;
+	while (fgets(line, SIZE, f) != NULL) {
+		printf("\n LINE: %i",line_count);
+		rowlen = strlen(line);
+		//loop_str(string);
+		cur = get_index(line,'m');
+		while (cur<rowlen) {
+			cur++;
+			if (line[cur]==40) {
+				if (cur >= 3 && line[cur-1]==108 && line[cur-2]==117 && line[cur-3]==109) {
+					mul_flag = 1;
+					//struct str_val a = {.c = "0"};
+					//struct str_val b = {.c = "0"};
+					//if (DEBUG) {
+					printf("\n 'mul(' found at cur %d", cur);
+					//}
+				};
+				continue; 
+			}
+			if (mul_flag) {
+				if (DEBUG) {
+					printf("\n mul flag set, look for int or ','.");
 				}
-			// TODO unset mul flag if ')', if neither ')' number or ',' erase a & b values.
-			}
-			 else if (string[cur]==41) {
-				printf("\n cparen encountered, close mul flag, store parameters as results for now");
-				delim_flag=0;
-				mul_flag=0;
+				char tmp = line[cur];
+				//b = concat(&tmp, a);
+				if (expect_int(&tmp)) {
+					if (DEBUG) {
+
+						printf("\n %c <- this is an integer.",line[cur]);
+					}
+					if(!delim_flag) {
+						inc++;
+						strcat(a,&tmp);
+						if (DEBUG) {
+							printf("\n value of 'a': %s",a);
+						}
+					} else {
+						strcat(b,&tmp);
+						if (DEBUG) {
+							printf("\n value of 'b': %s",b);
+						}
+					}	
+				}
+				else if (line[cur]==44) {
+					if (DEBUG) {
+						printf("\n %c  delimeter. Expecting next argument",line[cur]);
+					}	
+					if (!delim_flag) {
+						for (int i = 0; i > inc; i++) {
+							printf("\n USDHJFKLJS %c", line[cur-i]);
+						}
+						inc = 0;
+						delim_flag=1;
+					} else {
+						if (DEBUG) {
+							printf("\n UNEXPECTED token ',' at index %i. Break", cur);
+						}
+						mul_flag=0;
+						delim_flag=0;
+						memset(a, 0, sizeof(a));
+						memset(b, 0, sizeof(b));
+					}
+					// TODO unset mul flag if ')', if neither ')' number or ',' erase a & b values.
+				}
+				else if (line[cur]==41) {
+					//if (DEBUG) {
+					printf("\n cparen encountered gracefully, close mul flag, store parameters as results for now");
+					//}
+					delim_flag=0;
+					mul_flag=0;
+					int tmp_res = (atoi(a) * atoi(b));
+					res += tmp_res;
+					printf("\n RESULT of %i, %i multiplied: %i",atoi(a),atoi(b),tmp_res);
+					memset(a, 0, sizeof(a));
+					memset(b, 0, sizeof(b));
+				} else {
+					delim_flag=0;
+					mul_flag=0;
+					memset(a, 0, sizeof(a));
+					memset(b, 0, sizeof(b));
+					printf("\n UNEXPECTED token '%c' ",line[cur]);
+				}
 			}
 		}
-	} 
+		line_count++;
+		printf("\n RESULT THIS LINE: %i",res);
+	}
+	printf("\n TOTAL RESULT: %i",res);
 	free(a);
+	free(b);
 	return 0;
 }
