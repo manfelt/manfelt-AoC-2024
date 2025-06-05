a49730671747aeffc94dca08db39ddf7a60aa3e7
Magnus Manfelt <35919896+manfelt@users.noreply.github.com>
2025-06-05 20:19:01 +0200

    Filestream

--- 4.c
+++ 4.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-int main () {
+#define SIZE 1000
+
+FILE *f;
+char line[SIZE];
+
+int r(void) {
+	printf("fdds");
+	if (1) {
+		printf("true");
+	} else printf("false");
+}
+
+
+int main (void) {
+
+	f = fopen("4_input.txt", "r");
+
+	if (f == NULL) {
+		printf("Error opening file %p \n", f);
+		return 1;
+	}	
+
+
+	while (fgets(line, SIZE, f) != NULL) {
+		int rowlen = strlen(line);
+		int cur = 0;
+
+		while (cur<rowlen) {
+			//printf("%c",line[cur]);
+
+			cur++;
+		
+		}
+
+	}
+
+	// TODO reversj
+
+
 	return 0;
 }
